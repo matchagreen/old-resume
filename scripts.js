6e6847f44afc187b56336fe6fa9c9939608073d6
Alan Perez <alanperezse@gmail.com>
2021-04-28 17:03:03 -0600

    Resume Website
    1.0

---
+++ scripts.js
@@ -0,0 +1,43 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html');
+  const modalButton = document.getElementById('modal-button');
+  const modalAboutMe = document.getElementById('modal-about-me')
+  const modalNotification = document.getElementById('modal-notification')
+
+  modalNotification.classList.add('is-active');
+  html.classList.add('is-clipped');
+
+
+  modalButton.addEventListener('click', () => {
+    modalAboutMe.classList.toggle('is-active');
+    html.classList.toggle('is-clipped');
+  });
+
+  // Get all modal-background elements
+  const $modalBackgrounds = Array.prototype.slice.call(document.querySelectorAll('.modal-background'), 0);
+
+  $modalBackgrounds.forEach(el => {
+    el.addEventListener('click', () => {
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      $target.classList.remove('is-active')
+      html.classList.remove('is-clipped')
+    });
+  });
+
+  // Get all modal-background elements
+  const $modalClose = Array.prototype.slice.call(document.querySelectorAll('.modal-close'), 0);
+
+  $modalClose.forEach(el => {
+    el.addEventListener('click', () => {
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      $target.classList.remove('is-active')
+      html.classList.remove('is-clipped')
+    });
+  });
+});
